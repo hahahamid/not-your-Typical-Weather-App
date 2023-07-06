80753866f97f712995c0d91a60e0ece594224b33
Miran Hamid <miranhamid2002@gmail.com>
2023-07-06 18:53:32 +0530

    First Commit finally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        Get this project on
+        {" "}
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+        Github
+        </a>{" "}
+        | Hamid's work of Art {" "}
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    key: "863818c9fdc0db67e3af12f411178af5",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
